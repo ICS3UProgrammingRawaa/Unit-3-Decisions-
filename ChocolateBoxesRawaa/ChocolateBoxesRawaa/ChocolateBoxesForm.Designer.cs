83b68eed5af77cd8bb920f345ea823c342954789
Rawaa Al Ghanai <60893178+s458183@users.noreply.github.com>
2020-04-07 15:46:08 -0400

    Update

---
+++ ChocolateBoxesRawaa/ChocolateBoxesRawaa/ChocolateBoxesForm.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace ChocolateBoxesRawaa
+{
+    partial class frmChocolateBoxes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nudNumberOfBoxes = new System.Windows.Forms.NumericUpDown();
+            this.lblReward = new System.Windows.Forms.Label();
+            this.btnReward = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.nudNumberOfBoxes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // nudNumberOfBoxes
+            // 
+            this.nudNumberOfBoxes.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nudNumberOfBoxes.Location = new System.Drawing.Point(607, 200);
+            this.nudNumberOfBoxes.Name = "nudNumberOfBoxes";
+            this.nudNumberOfBoxes.Size = new System.Drawing.Size(182, 38);
+            this.nudNumberOfBoxes.TabIndex = 0;
+            // 
+            // lblReward
+            // 
+            this.lblReward.AutoSize = true;
+            this.lblReward.Font = new System.Drawing.Font("Microsoft Tai Le", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblReward.Location = new System.Drawing.Point(471, 413);
+            this.lblReward.Name = "lblReward";
+            this.lblReward.Size = new System.Drawing.Size(59, 40);
+            this.lblReward.TabIndex = 1;
+            this.lblReward.Text = "???";
+            // 
+            // btnReward
+            // 
+            this.btnReward.Font = new System.Drawing.Font("Microsoft Tai Le", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReward.Location = new System.Drawing.Point(132, 192);
+            this.btnReward.Name = "btnReward";
+            this.btnReward.Size = new System.Drawing.Size(291, 56);
+            this.btnReward.TabIndex = 2;
+            this.btnReward.Text = "Claim Reward ";
+            this.btnReward.UseVisualStyleBackColor = true;
+            this.btnReward.Click += new System.EventHandler(this.btnReward_Click);
+            // 
+            // frmChocolateBoxes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1002, 618);
+            this.Controls.Add(this.btnReward);
+            this.Controls.Add(this.lblReward);
+            this.Controls.Add(this.nudNumberOfBoxes);
+            this.Name = "frmChocolateBoxes";
+            this.Text = "Chocolate Boxes By Rawaa";
+            this.Load += new System.EventHandler(this.frmChocolateBoxes_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nudNumberOfBoxes)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown nudNumberOfBoxes;
+        private System.Windows.Forms.Label lblReward;
+        private System.Windows.Forms.Button btnReward;
+    }
+}
+
---
+++ ChocolateBoxesRawaa/ChocolateBoxesRawaa/ChocolateBoxesForm.cs
@@ -0,0 +1,64 @@
+﻿/*
+ * Created by: Rawaa Al Ghanai
+ * Created on: April 3rd, 2020
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #19 - Chocolate Boxes
+ * This program that rewards the user according to how many chocolates they sell. 
+ * If they sell over 20 boxes, they get a "prize". 
+ * If a they sells less than 20 boxes but more then 10, they get "honorable mention" . 
+ * Otherwise, they get a "small prize" (less then 10 boxes)
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChocolateBoxesRawaa
+{
+    public partial class frmChocolateBoxes : Form
+    {
+        public frmChocolateBoxes()
+        {
+            InitializeComponent();
+
+            // hide the reward statement
+            lblReward.Hide();
+        }
+
+        private void frmChocolateBoxes_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnReward_Click(object sender, EventArgs e)
+        {
+            // declare variables 
+            int soldBoxes;
+
+            // convert the numeric up and down value into an integer 
+            soldBoxes = Convert.ToInt32(nudNumberOfBoxes.Value);
+
+            // if statement that presents your reward according to how much the user was able to sell
+            if (soldBoxes > 20)
+            {
+                this.lblReward.Text = "Claim Your Large Prize!";
+                lblReward.Show();
+            }
+            else if (soldBoxes < 20 && soldBoxes > 10)
+            {
+                this.lblReward.Text = "You've Gotten Honorable Mentions!";
+                lblReward.Show();
+            }
+            else
+            {
+                this.lblReward.Text = "Claim Your Small Prize!";
+                lblReward.Show();
+            }
+        }
+    }
+}
