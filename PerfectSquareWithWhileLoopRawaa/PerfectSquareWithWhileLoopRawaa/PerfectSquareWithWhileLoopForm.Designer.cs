d0e68040fba3633eeeca7b8bfce67ed51895d8d2
Rawaa Al Ghanai <60893178+s458183@users.noreply.github.com>
2020-04-09 20:17:58 -0400

    Update

---
+++ PerfectSquareWithWhileLoopRawaa/PerfectSquareWithWhileLoopRawaa/PerfectSquareWithWhileLoopForm.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace PerfectSquareWithWhileLoopRawaa
+{
+    partial class frmPerfectSquareWithWhileLoop
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lstPerfectSquare = new System.Windows.Forms.ListBox();
+            this.nudEndValue = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.nudEndValue)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(450, 104);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(147, 51);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lstPerfectSquare
+            // 
+            this.lstPerfectSquare.FormattingEnabled = true;
+            this.lstPerfectSquare.ItemHeight = 20;
+            this.lstPerfectSquare.Location = new System.Drawing.Point(365, 264);
+            this.lstPerfectSquare.Name = "lstPerfectSquare";
+            this.lstPerfectSquare.Size = new System.Drawing.Size(332, 244);
+            this.lstPerfectSquare.TabIndex = 1;
+            // 
+            // nudEndValue
+            // 
+            this.nudEndValue.Location = new System.Drawing.Point(461, 193);
+            this.nudEndValue.Name = "nudEndValue";
+            this.nudEndValue.Size = new System.Drawing.Size(120, 26);
+            this.nudEndValue.TabIndex = 2;
+            // 
+            // frmPerfectSquareWithWhileLoop
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1091, 738);
+            this.Controls.Add(this.nudEndValue);
+            this.Controls.Add(this.lstPerfectSquare);
+            this.Controls.Add(this.btnStart);
+            this.Name = "frmPerfectSquareWithWhileLoop";
+            this.Text = "Perfect Square By Rawaa";
+            ((System.ComponentModel.ISupportInitialize)(this.nudEndValue)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ListBox lstPerfectSquare;
+        private System.Windows.Forms.NumericUpDown nudEndValue;
+    }
+}
+
---
+++ PerfectSquareWithWhileLoopRawaa/PerfectSquareWithWhileLoopRawaa/PerfectSquareWithWhileLoopForm.cs
@@ -0,0 +1,68 @@
+﻿/*
+ * Created by: Rawaa Al Ghanai
+ * Created on: April 7th, 2020
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day 22 - Perfect Square with While Loop
+ * This program tells the user all the possible perfect squares, 
+ * until it reaches the user's chosen number. 
+ * This program uses a loop to ensure all the possible perfect squares 
+ * do number exceed the maximum number the user chose. 
+ * It also uses an IF statement to ensure that the values are perfect squares.
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PerfectSquareWithWhileLoopRawaa
+{
+    public partial class frmPerfectSquareWithWhileLoop : Form
+    {
+        public frmPerfectSquareWithWhileLoop()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            // declare local varaibles 
+            int EndingValue, value, squareRootAsInteger;
+            double squareRootAsDouble;
+
+            // set the initial values
+            value = 0;
+
+            // clear all items in listbox
+            lstPerfectSquare.ClearSelected();
+
+            // get user's end value from numeric up down
+            EndingValue = Convert.ToInt32(this.nudEndValue.Value);
+
+            // state any perfect square between the initial value and the user's chosen end value
+            while (value <= EndingValue)
+            {
+                // take the square root of the value
+                squareRootAsDouble = Math.Sqrt(value);
+
+                // convert from double to integer 
+                squareRootAsInteger = Convert.ToInt32(squareRootAsDouble);
+
+                if (squareRootAsInteger == squareRootAsDouble)
+                {
+                    // add the perfect square to the list box 
+                    this.lstPerfectSquare.Items.Add(value + "    this is a perfect square");
+
+                    // refresh the form
+                    this.Refresh();
+                }
+                // the code to increment the value by 1 
+                value = value + 1;
+            }
+        }
+    }
+}
